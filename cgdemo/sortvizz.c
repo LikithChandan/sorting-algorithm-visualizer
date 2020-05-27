c996b224a3ee1fcce28401488f1cda0d98759ea7
AG <anirudh.600@gmail.com>
2020-05-27 12:34:44 +0530

    Recfactor code to C language

--- cgdemo/sortvizz.c
+++ cgdemo/sortvizz.c
@@ -1,32 +1,28 @@
-#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL\glut.h>
-#include <string>
 
 #define SORT_NO 4
-#define MAX 50			// Max Array size
-#define SPEED 800		// Speed of sorting
+#define MAX 50			// Array size
 
 int a[MAX];				// Array to sort
-int swapflag = 0;		// Flag to check if swapping has occurred
-int i = 0, j = 0;		// To iterate through the array
-int k = 0;				// To Switch from Welcome screen to Main Screen
+int swapflag = 0;
+int i = 0, j = 0, k = 0;
 int flag = 0;			// For insertion sort
 int sorting = 0;		// 1 if Sorted else 0
 int sort_count = 0;		// To cycle through the string
 
-std::string sort_string[] = { "Bubble Sort", "Selection Sort", "Insertion Sort", "Radix Sort" };
+char* sort_string[] = { "Bubble Sort", "Selection Sort", "Insertion Sort", "Radix Sort" };
 
 // Function to display text on screen char by char
-void bitmap_output(int x, int y, std::string str, void* font)
+void bitmap_output(int x, int y, char* string, void* font)
 {
 	int len, i;
 
 	glRasterPos2f(x, y);
-	len = str.length();
+	len = (int)strlen(string);
 	for (i = 0; i < len; i++) {
-		glutBitmapCharacter(font, str[i]);
+		glutBitmapCharacter(font, string[i]);
 	}
 }
 
@@ -64,13 +60,13 @@ void front()
 	bitmap_output(330, 525, "TO", GLUT_BITMAP_TIMES_ROMAN_24);
 	glVertex2f(325, 521);
 	glVertex2f(360, 521);
-	
+
 	bitmap_output(200, 485, "SORT VIZZ - A SORTING VISUALIZER", GLUT_BITMAP_TIMES_ROMAN_24);
 	glColor3f(0.0, 0.0, 1.0);
 	bitmap_output(30, 125, "Created By : ", GLUT_BITMAP_HELVETICA_18);
 	bitmap_output(30, 95, "Anirudh G - 1RN17CS013", GLUT_BITMAP_HELVETICA_18);
 	bitmap_output(30, 65, "Likith Chandan - 1RN17CS047", GLUT_BITMAP_HELVETICA_18);
-	
+
 	glColor3f(0.0, 0.0, 1.0);
 	glBegin(GL_QUADS);
 	glVertex2f(520, 110.0); glVertex2f(520, 150); glVertex2f(796, 150); glVertex2f(796, 110.0);
@@ -120,8 +116,8 @@ void display()
 	else
 	{
 		display_text();
-		std::string text;
-
+		
+		char text[10];
 		for (ix = 0; ix < MAX; ix++)
 		{
 			glColor3f(0, 0, 1);
@@ -132,7 +128,7 @@ void display()
 			glVertex2f(10 + (700 / (MAX + 1)) * ix, 50 + a[ix] * 4);
 			glEnd();
 
-			text = std::to_string(a[ix]);
+			_itoa(a[ix], text, 10);
 			glColor3f(0, 0, 0.8);
 			bitmap_output(12 + (700 / (MAX + 1)) * ix, 35, text, GLUT_BITMAP_TIMES_ROMAN_10);
 		}
@@ -169,8 +165,9 @@ void bubble_sort()
 				return;
 			}
 			j++;
-			if (j == MAX - 1) j = 0;
-			std::cout<<"swap "<<a[j]<<" and "<< a[j + 1]<<std::endl;
+			if (j == MAX - 1) 
+				j = 0;
+			printf("swap %d and %d\n", a[j], a[j + 1]);
 		}
 	}
 	sorting = 0;
@@ -198,7 +195,7 @@ void insertion_sort()
 			{
 				flag = 0;
 			}
-			std::cout << "swap "<< a[j] << " and " << a[j + 1] << std::endl;
+			printf("swap %d and %d\n", a[j], a[j + 1]);
 		}
 		i++;
 	}
@@ -247,7 +244,7 @@ int exponent = 1, pass = 0;
 
 void radix_sort()
 {
-	if (notsorted()) 
+	if (notsorted())
 	{
 		pass++;
 		int output[MAX];
@@ -265,9 +262,9 @@ void radix_sort()
 		}
 		for (i = 0; i < MAX; i++)
 			a[i] = output[i];
-		std::cout << "\nAfter pass " << pass << "\n";
+		printf("\nAfter pass %d: \n", pass);
 		for (int i = 0; i < MAX; i++)
-			std::cout << a[i] << " ";
+			printf("%d ", a[i]);
 		glutPostRedisplay();
 		exponent *= 10;
 		sorting = 0;
@@ -280,20 +277,20 @@ void radix_sort()
 }
 
 //Timer function to choose sort
-void makedelay(int)
+void makedelay(int i)
 {
 	if (sorting)
 	{
 		switch (sort_count)
 		{
-			case 0:	bubble_sort();		break;
-			case 1:	selection_sort();	break;
-			case 2: insertion_sort();	break;
-			case 3: radix_sort();		break;
+		case 0:	bubble_sort();		break;
+		case 1:	selection_sort();	break;
+		case 2: insertion_sort();	break;
+		case 3: radix_sort();		break;
 		}
 	}
 	glutPostRedisplay();
-	glutTimerFunc(SPEED / MAX, makedelay, 1);
+	glutTimerFunc(20, makedelay, 1);
 }
 // Keyboard Function
 void keyboard(unsigned char key, int x, int y)
@@ -304,10 +301,10 @@ void keyboard(unsigned char key, int x, int y)
 	{
 		switch (key)
 		{
-			case 27:	exit(0); //ESC
-			case 's':	sorting = 1; break;
-			case 'r':	system("cls"); exponent = 1, pass = 0; Initialize(); break;
-			case 'c':	sort_count = (sort_count + 1) % SORT_NO;	break;	//cycle between 0 to 3
+		case 27:	exit(0); //ESC
+		case 's':	sorting = 1; break;
+		case 'r':	system("cls"); exponent = 1, pass = 0; Initialize(); break;
+		case 'c':	sort_count = (sort_count + 1) % SORT_NO;	break;	//cycle between 0 to 3
 		}
 	}
 	if (k == 1 && sorting == 1)
